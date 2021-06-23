d1e519cabbd478868d938348562ffc811b063555
qqyurr <byyulli16@gmail.com>
2021-06-23 16:02:52 +0900

    에러 수정

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Form, Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
+  const [email] = useState('');
+  const [nickname] = useState('');
+  const [password] = useState('');
+  const [passwordCheck] = useState('');
+  const onChangeEmail = useCallback(() => {}, []);
+  const onChangeNickname = useCallback(() => {}, []);
+  const onChangePassword = useCallback(() => {}, []);
+  const onChangePasswordCheck = useCallback(() => {}, []);
+  const onSubmit = useCallback(() => {}, []);
+
   return (
     <div id="container">
       <Header>Sleact</Header>
@@ -35,10 +45,10 @@ const SignUp = () => {
               onChange={onChangePasswordCheck}
             />
           </div>
-          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
           {signUpError && <Error>이미 가입된 이메일입니다.</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
