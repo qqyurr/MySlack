1a8f49ec018d5176e1d26ed23f8b21bb240ea88d
qqyurr <byyulli16@gmail.com>
2021-06-30 16:38:33 +0900

    modal, menu, toggle

---
+++ front/components/Menu/index.tsx
@@ -0,0 +1,36 @@
+import { CreateMenu, CloseModalButton } from '@components/Menu/styles';
+import React, { CSSProperties, FC, PropsWithChildren, useCallback } from 'react';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<PropsWithChildren<Props>> = ({ closeButton, style, show, children, onCloseModal }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+  return (
+    // 내 부모를 누르면 창이 닫혀야하기 때문에 CreateMenu 태그에다가 onClick={onCloseModal} 모달 닫는 함수를 달아준다.
+    <CreateMenu onClick={onCloseModal}>
+      {/* stopPropagation으로 부모를 클릭했을 때는 닫히지만 나 자신을 클릭했을 때는 닫히지 않게 */}
+      {/* HTML에서는 우너래 이벤트 버블링이 된다. 원래는 자식을 클릭하면 부모까지 클릭이벤트가 전달이되는데 stopPropagation이 그걸 막아줍니다. */}
+      <div onClick={stopPropagation} style={style}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+// props의 기본값 설정
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -13,15 +13,17 @@ import {
   Chats,
   MenuScroll,
 } from '@layouts/Workspace/styles';
+import Menu from '@components/Menu';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Redirect } from 'react-router';
 import useSWR from 'swr';
 import gravatar from 'gravatar';
 // Channel의 index.tsx에서 Workspace 태그안에 있는 div 태그가 children이 된다.
 // 다른 컴포넌트 안에 넣은 JSX은 children이 된다.
 const Workspace: FC = ({ children }) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
     dedupingInterval: 2000, //2초, 캐시의 유지기간
     // dedupingInterval 기간 안에는 아무리 요청을 많이해도 서버로 요청가지 않고 첫번쨰 요청했을 때 들어온 response를 사용한다.
@@ -38,6 +40,11 @@ const Workspace: FC = ({ children }) => {
       });
   }, []);
 
+  const onClickUserProfile = useCallback(() => {
+    //toggle
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
   if (!data) {
     return <Redirect to="/login" />;
   }
@@ -46,12 +53,23 @@ const Workspace: FC = ({ children }) => {
     <div>
       <Header>
         <RightMenu>
-          <span>
+          <span onClick={onClickUserProfile}>
             <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}></ProfileImg>
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                <ProfileModal>
+                  <img src={gravatar.url(data.email, { s: '36px', d: 'retro' })} alt={data.nickname} />
+                  <div>
+                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-active">Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+              </Menu>
+            )}
           </span>
         </RightMenu>
       </Header>
-      <button onClick={onLogout}>로그아웃</button>
       <WorkspaceWrapper>
         <Workspaces>test</Workspaces>
         <Channels>
