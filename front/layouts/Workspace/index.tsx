91b3186663cf5367575d97f58d58e13355c0708a
qqyurr <byyulli16@gmail.com>
2021-06-30 19:17:47 +0900

    workspace 생성 모달

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -1,30 +1,54 @@
+// import ChannelList from '@components/ChannelList';
+// import CreateChannelModal from '@components/CreateChannelModal';
+// import DMList from '@components/DMList';
+// import InviteWorkspaceModal from '@components/InviteWorkspaceModal';
+import Menu from '@components/Menu';
+import Modal from '@components/Modal';
+import useInput from '@hooks/useInput';
+// import useSocket from '@hooks/useSocket';
+import Channel from '@pages/Channel';
+import DirectMessage from '@pages/DirectMessage';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import gravatar from 'gravatar';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import useSWR from 'swr';
+
 import {
+  AddButton,
+  Channels,
+  Chats,
   Header,
+  LogOutButton,
+  MenuScroll,
   ProfileImg,
   ProfileModal,
   RightMenu,
-  LogOutButton,
   WorkspaceButton,
-  WorkspaceName,
   WorkspaceModal,
-  WorkspaceWrapper,
+  WorkspaceName,
   Workspaces,
-  Channels,
-  Chats,
-  MenuScroll,
-} from '@layouts/Workspace/styles';
-import Menu from '@components/Menu';
-import fetcher from '@utils/fetcher';
-import axios from 'axios';
-import React, { FC, useCallback, useState } from 'react';
-import { Redirect } from 'react-router';
-import useSWR from 'swr';
-import gravatar from 'gravatar';
+  WorkspaceWrapper,
+} from './styles';
 // Channel의 index.tsx에서 Workspace 태그안에 있는 div 태그가 children이 된다.
 // 다른 컴포넌트 안에 넣은 JSX은 children이 된다.
 const Workspace: FC = ({ children }) => {
   const [showUserMenu, setShowUserMenu] = useState(false);
-  const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
+  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
+  const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('');
+  const [newUrl, onChangeNewUrl, setNewUrl] = useInput('');
+  const {
+    data: userData,
+    error,
+    revalidate,
+    mutate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher, {
     dedupingInterval: 2000, //2초, 캐시의 유지기간
     // dedupingInterval 기간 안에는 아무리 요청을 많이해도 서버로 요청가지 않고 첫번쨰 요청했을 때 들어온 response를 사용한다.
   });
@@ -40,12 +64,59 @@ const Workspace: FC = ({ children }) => {
       });
   }, []);
 
+  const onCloseUserProfile = useCallback((e) => {
+    e.stopPropagation();
+    setShowUserMenu(false);
+  }, []);
+
   const onClickUserProfile = useCallback(() => {
     //toggle
     setShowUserMenu((prev) => !prev);
   }, []);
 
-  if (!data) {
+  const onClickCreateWorkspace = useCallback(() => {
+    setShowCreateWorkspaceModal(true);
+  }, []);
+
+  const onCreateWorkspace = useCallback(
+    (e) => {
+      //form은 항상 e.preventDefault() -> 새로고침 안되게
+      e.preventDefault();
+      // !newWorkspace 이것만 하면 띄어쓰기 하나했을때도 통과됩니다
+      // 띄어쓰기까지 막으려면 .trim() 도 함께 검사
+      if (!newWorkspace || !newWorkspace.trim()) return;
+      if (!newUrl || !newUrl.trim()) return;
+      axios
+        .post(
+          'http://localhost:3095/api/workspaces',
+          {
+            workspace: newWorkspace,
+            url: newUrl,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          revalidate();
+          setShowCreateWorkspaceModal(false);
+          // 완료되고 나서는 input창 비워놓기
+          setNewWorkspace('');
+          setNewUrl('');
+        })
+        .catch((err) => {
+          console.dir(err);
+          toast.error(err.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newWorkspace, newUrl],
+  );
+
+  const onCloseModal = useCallback(() => {
+    setShowCreateWorkspaceModal(false);
+  }, []);
+
+  if (!userData) {
     return <Redirect to="/login" />;
   }
 
@@ -54,13 +125,16 @@ const Workspace: FC = ({ children }) => {
       <Header>
         <RightMenu>
           <span onClick={onClickUserProfile}>
-            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}></ProfileImg>
+            <ProfileImg
+              src={gravatar.url(userData.email, { s: '28px', d: 'retro' })}
+              alt={userData.nickname}
+            ></ProfileImg>
             {showUserMenu && (
-              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onCloseUserProfile}>
                 <ProfileModal>
-                  <img src={gravatar.url(data.email, { s: '36px', d: 'retro' })} alt={data.nickname} />
+                  <img src={gravatar.url(userData.email, { s: '36px', d: 'retro' })} alt={userData.nickname} />
                   <div>
-                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-name">{userData.nickname}</span>
                     <span id="profile-active">Active</span>
                   </div>
                 </ProfileModal>
@@ -71,13 +145,35 @@ const Workspace: FC = ({ children }) => {
         </RightMenu>
       </Header>
       <WorkspaceWrapper>
-        <Workspaces>test</Workspaces>
+        <Workspaces>
+          {userData?.Workspaces.map((ws) => {
+            return (
+              <Link key={ws.id} to={`/workspace/${123}/channel/일반`}>
+                <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
+              </Link>
+            );
+          })}
+          <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
+        </Workspaces>
         <Channels>
           <WorkspaceName>Sleact</WorkspaceName>
           <MenuScroll>menuscroll</MenuScroll>
         </Channels>
         <Chats>{children}</Chats>
       </WorkspaceWrapper>
+      <Modal show={showCreateWorkspaceModal} onCloseModal={onCloseModal}>
+        <form onSubmit={onCreateWorkspace}>
+          <Label id="workspace-label">
+            <span>워크스페이스 이름</span>
+            <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace} />
+          </Label>
+          <Label id="workspace-url-label">
+            <span>워크스페이스 url</span>
+            <Input id="workspace" value={newUrl} onChange={onChangeNewUrl} />
+          </Label>
+          <Button type="submit">생성하기</Button>
+        </form>
+      </Modal>
     </div>
   );
 };
