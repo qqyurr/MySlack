8a39fc2fe55930f507d9dc270c67332625aa2ceb
qqyurr <byyulli16@gmail.com>
2021-07-23 19:36:02 +0900

    DM 내용 표시하기

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,13 +1,22 @@
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
 import { IChat, IDM } from '@typings/db';
 import React, { useCallback, VFC, RefObject } from 'react';
-// import Chat from '@components/Chat';
+import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars';
 
-const ChatList = () => {
+interface Props {
+  chatData?: IDM[];
+}
+// 물음표 -> undefined과 null을 걸러줍니다
+
+const ChatList: VFC<Props> = ({ chatData }) => {
   return (
     <ChatZone>
-      <Section>section</Section>
+      <div>
+        {chatData?.map((chat) => {
+          return <Chat key={chat.id} data={chat} />;
+        })}
+      </div>
     </ChatZone>
   );
 };
