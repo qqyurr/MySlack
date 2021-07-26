39b7003b714b5f1469954dae8663c2ba952c83ea
qqyurr <byyulli16@gmail.com>
2021-07-26 21:23:21 +0900

    update

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,22 +1,22 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import gravatar from 'gravatar';
-import { Container, Header } from '@pages/DirectMessage/styles';
-import useSWR, { useSWRInfinite } from 'swr';
-import fetcher from '@utils/fetcher';
-import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
-import axios from 'axios';
+import useSocket from '@hooks/useSocket';
+import { Container, Header, DragOver } from '@pages/DirectMessage/styles';
 import { IDM } from '@typings/db';
+import fetcher from '@utils/fetcher';
 import makeSection from '@utils/makeSection';
+import axios from 'axios';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import gravatar from 'gravatar';
 import Scrollbars from 'react-custom-scrollbars';
-// import useSocket from '@hooks/useSocket';
-// import { DragOver } from '@pages/DirectMessage/styles';
+import { useParams } from 'react-router';
+import useSWR, { useSWRInfinite } from 'swr';
+
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
-  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
   const {
     data: chatData,
@@ -27,22 +27,20 @@ const DirectMessage = () => {
     (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
     fetcher,
   );
-  // 40
-  // 20 + 20 + 5 -> isEmpty 가 false
-  // 더 이상 가져올 정보 없으므로 isReachingEnd 는 true
-  const isEmpty = chatData?.[0]?.length === 0; // 데이터가 비어있다.
+  const [socket] = useSocket(workspace);
+  const isEmpty = chatData?.[0]?.length === 0;
   const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
   const scrollbarRef = useRef<Scrollbars>(null);
+  const [dragOver, setDragOver] = useState(false);
+
   const onSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
-      // optimistic UI
-      // 서버 쪽에 갔다오지 않았지만 성공해서 갔다온것처럼 미리 만들어놔야합니다.
+      console.log(chat);
       if (chat?.trim() && chatData) {
         const savedChat = chat;
         mutateChat((prevChatData) => {
           prevChatData?.[0].unshift({
-            // DM 객체
             id: (chatData[0][0]?.id || 0) + 1,
             content: savedChat,
             SenderId: myData.id,
@@ -53,29 +51,96 @@ const DirectMessage = () => {
           });
           return prevChatData;
         }, false).then(() => {
-          setChat(''); // 채팅 등록된 이후 채팅창에 있던 글자 지우기
-          scrollbarRef.current?.scrollToBottom(); // 채팅 치고 나서 제일 밑으로
+          setChat('');
+          scrollbarRef.current?.scrollToBottom();
         });
         axios
           .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
             content: chat,
           })
           .then(() => {
-            revalidate(); // 채팅 받아온 다음에 채팅이 등록되게
+            revalidate();
           })
           .catch(console.error);
       }
     },
     [chat, chatData, myData, userData, workspace, id],
   );
-  // 로딩시 스크롤바 제일 아래로
+
+  const onMessage = useCallback((data: IDM) => {
+    // id는 상대방 아이디
+    if (data.SenderId === Number(id) && myData.id !== Number(id)) {
+      mutateChat((chatData) => {
+        chatData?.[0].unshift(data);
+        return chatData;
+      }, false).then(() => {
+        if (scrollbarRef.current) {
+          if (
+            scrollbarRef.current.getScrollHeight() <
+            scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
+          ) {
+            console.log('scrollToBottom!', scrollbarRef.current?.getValues());
+            setTimeout(() => {
+              scrollbarRef.current?.scrollToBottom();
+            }, 50);
+          }
+        }
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    socket?.on('dm', onMessage);
+    return () => {
+      socket?.off('dm', onMessage);
+    };
+  }, [socket, onMessage]);
+
+  // 로딩 시 스크롤바 제일 아래로
   useEffect(() => {
     if (chatData?.length === 1) {
-      scrollbarRef.current?.scrollToBottom();
+      setTimeout(() => {
+        scrollbarRef.current?.scrollToBottom();
+      }, 100);
     }
   }, [chatData]);
 
-  // 정보가 없다면
+  const onDrop = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(e);
+      const formData = new FormData();
+      if (e.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (let i = 0; i < e.dataTransfer.items.length; i++) {
+          // If dropped items aren't files, reject them
+          if (e.dataTransfer.items[i].kind === 'file') {
+            const file = e.dataTransfer.items[i].getAsFile();
+            console.log('... file[' + i + '].name = ' + file.name);
+            formData.append('image', file);
+          }
+        }
+      } else {
+        // Use DataTransfer interface to access the file(s)
+        for (let i = 0; i < e.dataTransfer.files.length; i++) {
+          console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+          formData.append('image', e.dataTransfer.files[i]);
+        }
+      }
+      axios.post(`/api/workspaces/${workspace}/dms/${id}/images`, formData).then(() => {
+        setDragOver(false);
+        revalidate();
+      });
+    },
+    [revalidate, workspace, id],
+  );
+
+  const onDragOver = useCallback((e) => {
+    e.preventDefault();
+    console.log(e);
+    setDragOver(true);
+  }, []);
+
   if (!userData || !myData) {
     return null;
   }
@@ -83,16 +148,16 @@ const DirectMessage = () => {
   const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
-    <Container>
+    <Container onDrop={onDrop} onDragOver={onDragOver}>
       <Header>
-        <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
       <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+      {dragOver && <DragOver>업로드!</DragOver>}
     </Container>
   );
 };
-// Channel의 index.tsx에서 Workspace 태그안에 있는 div 태그가 Workspace의 children이 된다.
-// 다른 컴포넌트 안에 넣은 JSX은 children이 된다.
+
 export default DirectMessage;
